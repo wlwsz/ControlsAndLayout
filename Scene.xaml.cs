156e8a7e8510687d7b0824fb84faaf4a6184c2d1
wg <wg@LAPTOP-I1Q2VC1B>
2017-02-08 16:52:52 +0800

    添加项目文件。

---
+++ Scene.xaml.cs
@@ -0,0 +1,88 @@
+﻿// // Copyright (c) Microsoft. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace ControlsAndLayout
+{
+    public partial class Scene
+    {
+        public bool RealTimeUpdate = true;
+
+        private void HandleSelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            if (sender == null)
+                return;
+
+            Details.DataContext = (sender as ListBox).DataContext;
+        }
+
+        protected void HandleTextChanged(object sender, TextChangedEventArgs me)
+        {
+            if (RealTimeUpdate) ParseCurrentBuffer();
+        }
+
+        private void ParseCurrentBuffer()
+        {
+            try
+            {
+                var ms = new MemoryStream();
+                var sw = new StreamWriter(ms);
+                sw.Flush();
+                ms.Flush();
+                ms.Position = 0;
+                try
+                {
+                    var content = XamlReader.Load(ms);
+                    if (content != null)
+                    {
+                        cc.Children.Clear();
+                        cc.Children.Add((UIElement) content);
+                    }
+                }
+
+                catch (XamlParseException xpe)
+                {
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        protected void OnClickParseButton(object sender, RoutedEventArgs args)
+        {
+            ParseCurrentBuffer();
+        }
+
+        protected void ShowPreview(object sender, RoutedEventArgs args)
+        {
+            PreviewRow.Height = new GridLength(1, GridUnitType.Star);
+            CodeRow.Height = new GridLength(0);
+        }
+
+        protected void ShowCode(object sender, RoutedEventArgs args)
+        {
+            PreviewRow.Height = new GridLength(0);
+            CodeRow.Height = new GridLength(1, GridUnitType.Star);
+        }
+
+        protected void ShowSplit(object sender, RoutedEventArgs args)
+        {
+            PreviewRow.Height = new GridLength(1, GridUnitType.Star);
+            CodeRow.Height = new GridLength(1, GridUnitType.Star);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text=((ListBoxItem)listBox.SelectedItem).Content.ToString();
+
+        }
+    }
+}
